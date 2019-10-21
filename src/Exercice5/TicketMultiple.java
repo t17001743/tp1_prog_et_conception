c4601ee68940d9c7d8fb4c1f9305b69eed07637b
t17001743 <samantha.thiel@etu.univ-amu.fr>
2019-10-21 20:27:28 +0200

    svg

---
+++ src/Exercice5/TicketMultiple.java
@@ -0,0 +1,15 @@
+package Exercice5;
+
+public class TicketMultiple extends TicketRechargeable{
+
+    public TicketMultiple(int nbVoyages) {
+        super(nbVoyages);
+    }
+
+    /**
+     * Ne fait rien
+     */
+    public void recharge(){
+        return;
+    }
+}
