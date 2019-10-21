c6502d2bc42fc72f16725682a3d5cc79914edafe
t17001743 <samantha.thiel@etu.univ-amu.fr>
2019-10-21 16:05:21 +0200

    Exercice 6 fini

--- src/Exercice6/GrosMachin.java
+++ src/Exercice6/GrosMachin.java
@@ -2,7 +2,14 @@ package Exercice6;
 
 public class GrosMachin extends Machin{
 
+    public static boolean variable = false;
+
     public GrosMachin() {
         super();
+    }
+
+    public void glouglou(){
+        super.glouglou();
+        variable = true;
     }
 }
--- src/Exercice6/PetitBidule.java
+++ src/Exercice6/PetitBidule.java
@@ -1,12 +1,16 @@
 package Exercice6;
 
+import static Exercice6.GrosMachin.variable;
+
 public class PetitBidule extends Bidule{
 
     public PetitBidule() {
         super();
     }
 
-    /*public void bipbip(){
-
-    }*/
+    public void bipbip(){
+        if(variable) System.out.println("Glouglou");
+        else super.bipbip();
+        variable = false;
+    }
 }
